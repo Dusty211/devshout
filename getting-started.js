53759371be955cf01ae168c67a794db379bd8f3a
Dusty211 <kylebenz@hotmail.com>
2019-07-19 16:29:25 -0400

    added saved document

--- getting-started.js
+++ getting-started.js
@@ -5,10 +5,32 @@ mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
+
+  //Schema
   let kittySchema = new mongoose.Schema({
     name: String
   });
+
+  //Schema methods
+  kittySchema.methods.speak = function() {
+    let greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting)
+  }
+
+  //Model
   let Kitten = mongoose.model('Kitten', kittySchema);
+
+  //Document
   let silence = new Kitten({ name: 'Silence'});
   console.log(silence.name);
+
+  //Saved Document
+  let fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+  });
+
 });
