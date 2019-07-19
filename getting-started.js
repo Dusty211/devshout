f4ab48232196001a31d00246f1158d6e93834a5a
Dusty211 <kylebenz@hotmail.com>
2019-07-19 16:14:58 -0400

    schema, model, document. working.

--- getting-started.js
+++ getting-started.js
@@ -5,5 +5,10 @@ mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('test works')
+  let kittySchema = new mongoose.Schema({
+    name: String
+  });
+  let Kitten = mongoose.model('Kitten', kittySchema);
+  let silence = new Kitten({ name: 'Silence'});
+  console.log(silence.name);
 });
