1fb6915d46bf4275b6b85a033e5185fde1e17434
Dusty211 <kylebenz@hotmail.com>
2019-07-19 21:29:09 -0400

    middleware examples working

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
+//original static html homepage
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
 
-router.get('/', function(req, res, next) {
-  res.send('Hello World!');
-});
+//middleware examples
+const myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+const requestTime = function (req, res, next) {
+  req.requestTime = Date.now()
+  next()
+}
+
+router.use(myLogger)
+router.use(requestTime)
+
+//example route
+router.get('/', function (req, res) {
+  let responseText = 'Hello World!<br>'
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>'
+  res.send(responseText)
+})
+
+
+// router.get('/', function(req, res, next) {
+//   res.send('Hello World!');
+// });
 
 module.exports = router;
