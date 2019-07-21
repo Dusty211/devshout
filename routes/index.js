86d84d39227e6fc152a5c25b3c35af2215cb7e35
Dusty211 <kylebenz@hotmail.com>
2019-07-21 00:36:34 -0400

    working index with promises

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Shout = require('../db/models/shout')
 
-////original static html homepage
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-////middleware examples
-const myLogger = function (req, res, next) {
-  console.log('LOGGED')
-  next()
+const saveShout = function () {
+  return new Promise(function(resolve, reject){
+    const newshout = new Shout;
+    newshout.save(function (err, newshout) {
+      resolve();
+    })
+  });
 }
 
-const requestTime = function (req, res, next) {
-  req.requestTime = Date.now()
-  next()
+const allShouts = function () {
+  return new Promise(function(resolve, reject) {
+    Shout.find(function (err, shouts) {
+      resolve(shouts);
+    });
+  });
 }
 
-router.use(myLogger)
-router.use(requestTime)
-
-////example route
-router.get('/', function (req, res) {
-  // let responseText = 'Hello World!<br>'
-  // responseText += '<small>Requested at: ' + req.requestTime + '</small>'
-  // res.send(responseText)
-  res.json({
-    "TestArrayKey": [
-      {
-        "1": "one",
-        "2": "two",
-        "3": "three",
-      },
-      {
-        "4": "four",
-        "5": "five",
-        "6": "six",
-      },
-      {
-        "7": "seven",
-        "8": "eight",
-        "9": "nine",
-      },
-    ],
-  })
-})
-
-
-// router.get('/', function(req, res, next) {
-//   res.send('Hello World!');
-// });
+router.get('/', async function (req, res) {
+  await saveShout();
+  const response = await allShouts();
+  res.json(response);
+});
 
 module.exports = router;
