52279d7f7f18b43c7c80c239ccf677f8d361e5ec
Dusty211 <kylebenz@hotmail.com>
2019-07-23 12:19:42 -0400

    added mongoose.set('useCreateIndex', true) to dbConnector.js

--- db/dbConnector.js
+++ db/dbConnector.js
@@ -4,6 +4,7 @@ module.exports = class DbConnector {
 
   connect() {
     mongoose.connect('mongodb://localhost/gitshout', {useNewUrlParser: true});
+    mongoose.set('useCreateIndex', true);
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection errror:'));
     db.once('open', function() {
